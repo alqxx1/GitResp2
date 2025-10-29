3573c9e368c02378ca0130810fb132428760ce3a
alq <1077982932@qq.com>
2025-10-29 21:55:17 +0800

    我增加了年龄这个属性

---
+++ TestMoudle/src/alq/work/Git/Person.java
@@ -0,0 +1,11 @@
+package alq.work.Git;
+
+/**
+ * @Auther: alq
+ * @Date: 2025/10/29 - 10 - 29 - 21:53
+ * @Description: alq.work.Git
+ * @version: 1.0
+ */
+public class Person {
+    private String name;
+}
